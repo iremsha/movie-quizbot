ee0ff8024cebee42d6b0676fb97fdb5972d87796
An <an@.mail.ru>
2018-10-04 21:53:00 +0500

    test 5

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args){
         System.out.println("test 2");
-        System.out.println("test 3");
+        System.out.println("test 5");
     }
 }
